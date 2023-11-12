b65a626730a95e2a5a0471d3050321f8edd09017
Casshkem <118006551+Casshkem@users.noreply.github.com>
2023-11-12 01:12:51 -0500

    Create script.js
    Open API Js Call

---
+++ script.js
@@ -0,0 +1,40 @@
+const chatOutput = document.getElementById('chat-output');
+const messageInput = document.getElementById('message-input');
+
+function sendMessage() {
+    const userMessage = messageInput.value;
+    if (!userMessage) return;
+
+    displayMessage(userMessage, 'user');
+
+    // Replace 'YOUR_API_KEY' with your actual ChatGPT API key
+    const apiKey = 'YOUR_API_KEY';
+    const apiUrl = 'https://api.openai.com/v1/chat/completions';
+
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: userMessage }],
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const chatResponse = data.choices[0].message.content;
+        displayMessage(chatResponse, 'assistant');
+    })
+    .catch(error => console.error('Error:', error));
+
+    messageInput.value = '';
+}
+
+function displayMessage(message, role) {
+    const messageDiv = document.createElement('div');
+    messageDiv.className = role;
+    messageDiv.textContent = message;
+    chatOutput.appendChild(messageDiv);
+}
